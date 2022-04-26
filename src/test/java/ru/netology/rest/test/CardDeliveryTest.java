ef31cbbe0ae1d577e5210b21428220f179777c15
Anna Kuznetsova <akry67@gmail.com>
2022-04-26 22:12:25 +0400

    new

--- src/test/java/ru/netology/rest/test/CardDeliveryTest.java
+++ src/test/java/ru/netology/rest/test/CardDeliveryTest.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.experimental.UtilityClass;
 import lombok.val;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -23,51 +24,32 @@ import java.util.Locale;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
+import static ru.netology.rest.utils.DataGenerator.Registration.generateDate;
 
 public class CardDeliveryTest {
 
-    private static Faker faker;
-
-    String generateDate(int days) {
-
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
-    }
-
-
-    @BeforeAll
-    static void setUpAll() {
-        faker = new Faker(new Locale("ru"));
-    }
-
-    @UtilityClass
-    public static class Registration {
-        public static Generation generation(String locale) {
-            Faker faker = new Faker(new Locale(locale));
-            return new Generation(faker.address().city(), faker.date(), faker.name().fullName(), faker.phoneNumber().phoneNumber());
-        }
-    }
 
 
     @Test
     void shouldSubmitRequest() {
 
         String planningDate = generateDate(6);
+        String planningDateNew = generateDate(8);
 
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
-        $x("//input[@placeholder=\"Город\"]").setValue(faker.address().city());
+        $x("//input[@placeholder=\"Город\"]").setValue(DataGenerator.Registration.generateCity("ru"));
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $x("//input[@placeholder=\"Дата встречи\"]").val(planningDate);
-        $(By.name("name")).val(faker.name().name());
-        $(By.name("phone")).val(faker.phoneNumber().phoneNumber());
+        $(By.name("name")).val(DataGenerator.Registration.generateName("ru"));
+        $(By.name("phone")).val(DataGenerator.Registration.generatePhone("ru"));
         $("[data-test-id=\"agreement\"]").click();
         $(byText("Запланировать")).click();
         $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(15));
         $(byText("Запланировать")).click();
+        $x("//input[@placeholder=\"Дата встречи\"]").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $x("//input[@placeholder=\"Дата встречи\"]").val(planningDateNew);
         $(byText("Перепланировать")).click();
-        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate), Duration.ofSeconds(15));
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно запланирована на " + planningDateNew), Duration.ofSeconds(15));
     }
-
-
 }
--- src/test/java/ru/netology/rest/utils/DataGenerator.java
+++ src/test/java/ru/netology/rest/utils/DataGenerator.java
@@ -1,19 +1,52 @@
 package ru.netology.rest.utils;
 
+import com.github.javafaker.Address;
 import com.github.javafaker.Faker;
 import lombok.experimental.UtilityClass;
 import ru.netology.rest.generate.Generation;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 @UtilityClass
 public class DataGenerator {
 
+    private static Faker faker;
+
+    static void setUpAll() {
+        faker = new Faker(new Locale("ru"));
+    }
+
     @UtilityClass
-    public static class Registration{
-        public static Generation generation(String locale){
-            Faker faker = new Faker(new Locale(locale));
-            return new Generation(faker.address().city(), faker.date(), faker.name().fullName(), faker.phoneNumber().phoneNumber());
+    public static class Registration {
+
+        public static String generateDate(int days) {
+            // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
+            // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
+            return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        }
+
+        public static String generateCity(String locale) {
+            // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
+            // с помощью Faker, либо используя массив валидных городов и класс Random
+            String city = faker.address().city();
+            return city;
+        }
+
+        public static String generateName(String locale) {
+            // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
+            // использовать Faker
+            String name = faker.name().name();
+            return name;
+        }
+
+        public static String generatePhone(String locale) {
+            // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
+            // использовать Faker
+            String phone = faker.phoneNumber().phoneNumber();
+            return phone;
         }
     }
+
 }
